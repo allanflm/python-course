d2096103d616ede3534f8355d42a102b4fa9f3c3
Allan <allanfelipedk@gmail.com>
2024-02-01 21:57:34 -0300

    aula19.py

---
+++ aula17.py
@@ -0,0 +1,19 @@
+# if / elif ..... / else
+condiacao_1 = False
+condiacao_2 = True
+condiacao_3 = True
+
+if condiacao_1:
+    print("Condicao 1")
+elif condiacao_2:
+    print("elif 0")
+elif condiacao_3:
+    print("elif 1")
+else:
+    print("Condicao 1 falsa")
+
+
+if 10 == 9:
+    print("10 é igual a 10")
+elif 20 == 20:
+    print("20 é igual a 20")
