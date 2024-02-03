b108b4c05f6e5a2274a819fbba3bc6d1690d2f0a
Allan <allanfelipedk@gmail.com>
2024-02-03 19:34:45 -0300

    aula19 - operadores de comparação

---
+++ aula19.py
@@ -0,0 +1,18 @@
+"""
+    Operadores de comparação (Relacionais)
+    OP      significado        
+    >       Maior
+    <       Menor   
+    >=      Maior ou igual
+    <=      Menor ou igual
+    ==      igual
+    !=      Diferente       
+
+"""
+
+
+maior = 2 > 1
+print(maior)
+
+menor = 2 > 1
+print(menor)
