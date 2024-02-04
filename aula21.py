091b90edb68e260a9a9e357b36aa7b8e11f03945
Allan <allanfelipedk@gmail.com>
2024-02-04 09:55:03 -0300

    aula21 - operadores logicos - and

---
+++ aula21.py
@@ -0,0 +1,8 @@
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+senha_permitida = '1234'
+
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print("Entrando...")
+else:
+    print("Senha incorreta...")
