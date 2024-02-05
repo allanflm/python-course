b999ae404911ae5f5e09e26862d5a08421d34649
Allan <allanfelipedk@gmail.com>
2024-02-05 20:35:04 -0300

    aula26 - Formatação de strings com f-strings

---
+++ aula26.py
@@ -0,0 +1,11 @@
+"""
+Formatação de strings com f-strings
+- Conversion flags - !r !s !a
+
+"""
+variavel = 'ABC'
+
+print(variavel)
+print(f'{variavel}')
+print(f'{variavel:>10}')
+print(f"{variavel!r}")
