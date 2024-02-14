9dfd634a6a2a445c25f01d09b3daaa3030175373
Allan <allanfelipedk@gmail.com>
2024-02-13 21:32:37 -0300

    aula30 - Flags, is, is not e None

---
+++ aula31.py
@@ -0,0 +1,24 @@
+"""
+    Flag(bandeira) - Marca um local
+    None - não valor
+    is e is not - é ou não é (tipo, valor, identidade)
+    id = identidade
+"""
+
+# v1 = "a"
+# print(id(v1))
+# v2 = "a"
+# print(id(v2))
+condicao = False
+passou_no_if = None
+if condicao:
+    passou_no_if = True
+    print("Faça algo")
+else:
+    print("Não faça algo")
+
+
+if passou_no_if is None:
+    print("Não passou no if")
+else:
+    print("Passou no if")
