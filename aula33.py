cdb519f022ebcd312c970fc3c549f69fafdc9809
Allan <allanfelipedk@gmail.com>
2024-02-15 21:21:56 -0300

    aula33 - tipos built-in, documentação, tipo imutáveis, métodos de string

---
+++ aula33.py
@@ -0,0 +1,7 @@
+"""
+ https://docs.python.org/pt-br/3/library/stdtypes.html
+"""
+string = "allan"
+outra_variavel = f"{string[:3]}ABC{string[4:]}"
+print(outra_variavel)
+print(string.capitalize())
