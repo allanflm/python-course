299e0363e6f7676b87d1b7c04f9a7bde5ad4a47a
Allan <allanfelipedk@gmail.com>
2024-02-15 21:43:43 -0300

    aula35 -  while - Condição em detalhes

---
+++ aula35.py
@@ -0,0 +1,15 @@
+"""
+ -> Repetições:
+        - while(enquanto)
+            -> Executa uma ação enquanto uma condição for verdadeira!
+-> Loop infinito - quando um codigo nao tem fim!
+"""
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    # contador = contador + 1
+    contador += 1
+    # print(contador) -> vai até o 11. (1 - 11)
+
+print("Acabou!")
