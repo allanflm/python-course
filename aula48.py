6967eecd047e901f3c6bea85c7b99e0a39b4f4ad
Allan <allanfelipedk@gmail.com>
2024-02-23 19:27:27 -0300

    aula48 - tipo list - Introdução às listas mutáveis em Python

---
+++ aula48.py
@@ -0,0 +1,11 @@
+string = "ABCD"
+
+lista = [123, True, "Allan", 1.2, []]
+
+print(lista[2], type(lista[2]))
+print(lista[2].upper(), type(lista[2]))
+
+# -----------------------------------
+print(lista[-3])
+lista[-3] = "Felipe"
+print(lista[-3])
