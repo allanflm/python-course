50aa237e474d8caa32a99c842f32798c62b3e693
Allan <allanfelipedk@gmail.com>
2024-02-24 18:24:19 -0300

    mod: aula48 - Alterando uma lista com índices, del, append e pop (Tipo list)

--- aula48.py
+++ aula48.py
@@ -1,11 +1,32 @@
-string = "ABCD"
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+# string = "ABCD"
 
-lista = [123, True, "Allan", 1.2, []]
+# lista = [123, True, "Allan", 1.2, []]
 
-print(lista[2], type(lista[2]))
-print(lista[2].upper(), type(lista[2]))
+# print(lista[2], type(lista[2]))
+# print(lista[2].upper(), type(lista[2]))
 
-# -----------------------------------
-print(lista[-3])
-lista[-3] = "Felipe"
-print(lista[-3])
+# # -----------------------------------
+# print(lista[-3])
+# lista[-3] = "Felipe"
+# print(lista[-3])
+
+
+lista = [10, 20, 30, 40]
+print(lista)
+
+# - adicionado
+lista.append(50)
+print(lista)
+
+# removendo o ultimo valor
+print(f"ultimo valor removido -> {lista.pop()}")
