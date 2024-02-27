46b2ee1ddf5741e3af50da62b41a684b30833f13
Allan <allanfelipedk@gmail.com>
2024-02-27 20:27:20 -0300

    aula53 - enumerate - enumera iteraveis (indices)

---
+++ aula53.py
@@ -0,0 +1,19 @@
+"""
+enumerate - enumera iteraveis (indices)
+"""
+
+lista = ["Allan", "Felipe", "Malinoski"]
+lista.append("Thais")
+
+for indice, nome in enumerate(lista):
+    print(indice, nome, lista[indice])
+
+# for item in enumerate(lista):
+#     indice, nome = item
+#     print(indice, nome)
+
+
+# for tupla_enumerada in enumerate(lista):
+#     print('FOR da tupla:')
+#     for valor in tupla_enumerada:
+#         print(f'\t{valor}')
