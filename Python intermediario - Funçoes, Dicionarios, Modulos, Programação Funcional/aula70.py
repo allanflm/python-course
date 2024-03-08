6f1d9c385f12b979b6ab9d512e239f120e401010
Allan <allanfelipedk@gmail.com>
2024-03-08 20:50:37 -0300

    aula70 - Retorno de valores das funções (return)

---
+++ Python intermediario - Funçoes, Dicionarios, Modulos, Programação Funcional/aula70.py
@@ -0,0 +1,16 @@
+# Retorno de valores das funções (return)
+
+def soma(x, y):
+    if x > 10:
+        # return [10, 20]
+        return "x é maior do que 10"
+    return x + y
+
+
+# soma1 = soma(5, 6)
+# soma2 = soma(4, 6)
+# print(soma1 + soma2)
+
+somaM10 = soma(11, 55)
+# somaM10 = soma(10, 55)
+print(somaM10)
