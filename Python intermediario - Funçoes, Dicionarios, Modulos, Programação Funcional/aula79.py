ce26b90673018e5a7f2a3acac2ce6352226dd31b
Allan <allanfelipedk@gmail.com>
2024-03-13 20:05:57 -0300

    aula79 - exemplos de uso dos sets

---
+++ Python intermediario - Funçoes, Dicionarios, Modulos, Programação Funcional/aula79.py
@@ -0,0 +1,13 @@
+# exemplos de uso dos sets
+
+letras = set()
+
+while True:
+    letra = input("Digite: ")
+    letras.add(letra.lower())
+
+    if "l" in letras:
+        print("PARABENS!")
+        break
+
+    print(letras)
