bb17c1fd148cf2f656071796151207b42daf9317
Allan <allanfelipedk@gmail.com>
2024-03-18 19:14:06 -0300

    aula85 - List comprehension com mais de um for

---
+++ Python intermediario - Funçoes, Dicionarios, Modulos, Programação Funcional/aula85.py
@@ -0,0 +1,18 @@
+# https://www.youtube.com/watch?v=1YbTDczvqco
+lista = []
+for x in range(3):
+    for y in range(3):
+        lista.append((x, y))
+
+lista = [
+    (x, y)
+    for x in range(3)
+    for y in range(3)
+]
+
+lista = [
+    [(x, letra) for letra in "Allan"]
+    for x in range(3)
+]
+
+print(lista)
