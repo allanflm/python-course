295dc0969aa22a0341dc4b5602f4c1f2e564ffc5
Allan <allanfelipedk@gmail.com>
2024-03-19 19:57:45 -0300

    aula90 - Generator expression, Iterables e Iterators em Python

---
+++ Python intermediario - Funçoes, Dicionarios, Modulos, Programação Funcional/aula90.py
@@ -0,0 +1,12 @@
+# Generator expression, Iterables e Iterators em Python:
+
+import sys
+
+iterable = ["Eu", "Tenho", "__iter__"]
+iterator = iter(iterable)
+
+lista = [n for n in range(1000000)]
+generator = (n for n in range(1000000))
+
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
