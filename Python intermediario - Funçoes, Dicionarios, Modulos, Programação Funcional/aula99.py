5b3bcf7e9aa58a562be101b052d40428b9208f51
Allan <allanfelipedk@gmail.com>
2024-03-22 20:41:01 -0300

    aula99 - Introdução aos packages (pacotes) em Python

---
+++ Python intermediario - Funçoes, Dicionarios, Modulos, Programação Funcional/aula99.py
@@ -0,0 +1,9 @@
+import aula99_package.modulo
+from aula99_package import modulo
+from aula99_package.modulo import *
+
+print(soma_do_modulo(1, 3))
+print(aula99_package.modulo.soma_do_modulo(1, 3))
+print(modulo.soma_do_modulo(5, 6))
+print(variavel)
+print(nova_variavel)
