0323e2d0e815e86a28d5dcf1d5697b1738f0b50a
Allan <allanfelipedk@gmail.com>
2024-03-27 20:26:40 -0300

    aula107 - exercicio - zip e zip_longest do itertools

--- Python intermediario - Funçoes, Dicionarios, Modulos, Programação Funcional/aula107_exercicios.py
+++ Python intermediario - Funçoes, Dicionarios, Modulos, Programação Funcional/aula107_exercicios.py
@@ -2,6 +2,15 @@
 # Resultado
 # [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
 
+
 # def zipper(l1, l2):
 #     intervalo = min(len(l1), len(l2))
 #     return [(l1[i], l2[i]) for i in range(intervalo)]
+
+from itertools import zip_longest
+
+l1 = [('Salvador'), ('Ubatuba'), ('Belo Horizonte')]
+l2 = ['BA', 'SP', 'MG', 'RJ']
+# print(zipper(l1, l2))
+print(list(zip(l1, l2)))
+print(list(zip_longest(l1, l2, fillvalue="SEM CIDADE")))
