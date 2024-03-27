e91a50e087e2e695a5a591ca493a400557e46c07
Allan <allanfelipedk@gmail.com>
2024-03-27 20:16:18 -0300

    refactor: names

---
+++ Python intermediario - Funçoes, Dicionarios, Modulos, Programação Funcional/aula107_exercicios.py
@@ -0,0 +1,7 @@
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+# def zipper(l1, l2):
+#     intervalo = min(len(l1), len(l2))
+#     return [(l1[i], l2[i]) for i in range(intervalo)]
